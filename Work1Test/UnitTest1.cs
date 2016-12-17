55009d9ec5df5db71e1795cb3251ad497367886a
Igor <igorro2@ac.sce.ac.il>
2016-12-17 21:11:55 +0200

    Add test project

---
+++ Work1Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Work1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
